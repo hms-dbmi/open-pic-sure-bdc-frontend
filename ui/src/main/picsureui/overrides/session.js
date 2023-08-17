fe55d626d42daf87a7ec3658340fbe8c6d9eb34b
gcolon021 <gcolon021@gmail.com>
2023-08-17 09:41:18 -0400

    [ALS-4794] Update access without login

--- ui/src/main/picsureui/overrides/session.js
+++ ui/src/main/picsureui/overrides/session.js
@@ -1,6 +1,14 @@
-define([], function(){
+define([], function () {
+    let expired = function() {
+        if (sessionStorage.session){
+            return new Date().getTime()/1000 > JSON.parse(atob(JSON.parse(sessionStorage.session).token.split('.')[1])).exp;
+        }
+        //no session -> no token --> session has expired or does not exist.
+        return true;
+    };
+
     return {
-        handleQueryTemplateAndMeResponseSuccess: function(queryTemplateResponse, meResponse){
+        handleQueryTemplateAndMeResponseSuccess: function (queryTemplateResponse, meResponse) {
             var currentSession = JSON.parse(sessionStorage.getItem("session"));
             currentSession.queryTemplate = queryTemplateResponse[0].queryTemplate;
             currentSession.privileges = meResponse[0].privileges;
@@ -10,12 +18,14 @@ define([], function(){
 
             sessionStorage.setItem("session", JSON.stringify(currentSession));
 
-            // window.location = "/picsureui/";
+            window.location = '/picsureui/';
         },
-        handleNotAuthorizedResponse: function() {
+        handleNotAuthorizedResponse: function () {
             try {
-                if (new Date().getTime()/1000 > JSON.parse(atob(JSON.parse(sessionStorage.session).token.split('.')[1])).exp) {
-                    history.pushState({}, "", "/psamaui/logout");
+                if (expired()) {
+                    // We just need to clear the session and get a new token.
+                    sessionStorage.removeItem("session");
+                    window.location = '/psamaui/login';
                 } else {
                     history.pushState({}, "", "/psamaui/not_authorized");
                 }
